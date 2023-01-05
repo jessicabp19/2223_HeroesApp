15dbf79929d85fc9b08d276a41ce794eca22852e
Jessica Boton <jessicaboton@gmail.com>
2023-01-04 22:58:59 -0600

    ADD Heroes Routes and info

--- heroes-spa/src/heroes/components/HeroCard.jsx
+++ heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if( alter_ego === characters ) return (<></>);
+    // return <p>{ characters }</p>
+
+    return (alter_ego === characters)
+        ? <></>
+        : <p>{ characters }</p>
+}
 
 export const HeroCard = ({ 
     id,
@@ -9,6 +19,8 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+
+    //const charactersByHero = (<p>{ characters }</p>);
 
   return (
     <div className="col">
@@ -24,7 +36,21 @@ export const HeroCard = ({
                         <h5 className="card-title">{ superhero }</h5>
                         <p className="card-text">{ alter_ego }</p>
 
-                        <p>{ characters }</p>
+                        {/* {
+                            ( alter_ego !== characters ) && charactersByHero
+                            ( alter_ego !== characters ) && (<p>{ characters }</p>)
+                        } */}
+                        <CharactersByHero alter_ego={alter_ego} characters={characters} />
+
+                        <p className="card-text">
+                            <small className="text-muted">{ first_appearance }</small>
+                        </p>
+
+                        <Link to={`/hero/${ id }`}> 
+                            View more 
+                        </Link>
+
+                        
                     </div>
                 </div>
             </div>
--- heroes-spa/src/heroes/pages/HeroPage.jsx
+++ heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,59 @@
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+  
+  const hero = useMemo( () => getHeroById( id ), [ id ] ); ;
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
+  /**
+   * Cuando el componente lanza un cambio en el state (useState, useReducer, etc) (aunque sea en un componente padre)
+   * El componente se vuelve a disparar y las funciones llamadas tambien
+   * Podríamos memorizar el resultado si el id no cambio
+   * Así estaba antes: const hero = getHeroById( id );
+   * En este ejercicio no es necesario, pero lo implementamos para reforzar el tema
+   * Conexión a la bd, restful api endpoint, grabar en nuestro index db
+   */
+  
+  if( !hero ){
+    return <Navigate to="/marvel" />
+  }
+
   return (
-    <h1>HeroPage</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={ `/assets/heroes/${ id }.jpg` }
+          alt={ hero.superhero }
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego: </b> {hero.alter_ego} </li>
+          <li className="list-group-item"> <b>Publisher: </b> {hero.publisher} </li>
+          <li className="list-group-item"> <b>First Appearance:</b> {hero.first_appearance} </li>
+        </ul>
+
+        <h5 className="mt-3"> Characters </h5>
+        <p>{ hero.characters}</p>
+
+        <button 
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+        >
+          Back
+        </button>
+
+      </div>
+    </div>
   )
 }
